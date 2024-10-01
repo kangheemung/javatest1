f7a444f2b280fc4ce580aebfc31b561ac20a4d3d
kurotamashi30 <kurotamashi30@gmail.com>
2024-10-01 12:38:40 +0900

    java

---
+++ javatest2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        int A = 437326;
+        int B = 9085;
+        
+        int X = A / B;
+        int Y = A % B;
+        
+        System.out.println(X + " " +Y);
+    }
+}
